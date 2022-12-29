95d12230cd6f0e5ccbb7c13caaa3b2d1d13f9115
Pyrix25633 <biral.mattia@gmail.com>
2022-12-29 11:12:27 +0100

    Forge init

--- src/main/java/net/rupyber_studios/fbi_swat_armors/util/ModRegistries.java
+++
@@ -1,9 +0,0 @@
-package net.rupyber_studios.fbi_swat_armors.util;
-
-public class ModRegistries {
-    public static void registerAttributes() {
-    }
-
-    public static void registerRenderers() {
-    }
-}
